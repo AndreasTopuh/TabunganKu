3340f53ddb066f8c356f83cce041fa595cd8f799
AndreasTopuh <andreastopuh23@gmail.com>
2024-02-28 14:33:29 +0800

    adding first page Tabungan Ku

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Tabungan Ku</Text>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#02CF8E',
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    fontFamily: 'Poppins',
+    color: 'black',
+  },
+});
